bea052c429b11383da020df190dbc3f5eb8b9a3e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SLGridExample/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Resources;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using GridExample;
+using System.Collections.Generic;
+using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Printing;
+using DevExpress.Xpf.Core;
+
+
+namespace SLGridExample {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+
+            Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e) {
+            grid.ItemsSource = new List<TestData>() {
+                new TestData() { PlainText = "Mercedes-Benz SLK", MemoText = "LMA AG \n2004 \nSilver", BooleanMember = true, Image = GetImage("/SLGridExample;component/Images/1.jpg"), Price = 25 },
+                new TestData() { PlainText = "Rolls-Royce Corniche", MemoText ="Western Motors \n1975 \nSnowy whight", BooleanMember = false, Image = GetImage("/SLGridExample;component/Images/2.jpg"), Price = 15 },
+                new TestData() { PlainText = "Ford Ranger FX-4", MemoText = "Sun car rent\n1999 \nRed rock", BooleanMember = true, Image = GetImage("/SLGridExample;component/Images/3.jpg"), Price = 20 },
+            };
+        }
+
+        ImageSource GetImage(string path) {
+
+            StreamResourceInfo sr = Application.GetResourceStream(new Uri(path, UriKind.Relative));
+            BitmapImage bmp = new BitmapImage();
+            bmp.SetSource(sr.Stream);
+
+            return bmp;
+        }
+
+        private void PrintButton_Click(object sender, RoutedEventArgs e) {
+            ShowPrintPreview(grid);
+        }
+
+        private void ShowPrintPreview(GridControl grid) {
+            DocumentPreview preview = new DocumentPreview();
+            PrintableControlLink link = new PrintableControlLink(grid.View as IPrintableControl);
+            link.ExportServiceUri = "../ExportService.svc";
+            LinkPreviewModel model = new LinkPreviewModel(link);
+            preview.Model = model;
+
+            link.CreateDocument(true);
+            DXDialog dlg = new DXDialog();
+            dlg.Content = preview;
+            dlg.Height = 640;
+            dlg.Left = 150;
+            dlg.Top = 150;
+            dlg.ShowDialog();
+        }
+    }
+}
---
+++ CS/SLGridExample/Model/TestData.cs
@@ -0,0 +1,24 @@
+﻿// Developer Express Code Central Example:
+// How to use custom printing templates
+// 
+// This example demonstrates how to use grid's printing templates to change the
+// grid printing appearance.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E3228
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace GridExample {
+    public class TestData {
+        public string PlainText { get; set; }
+        public string MemoText { get; set; }
+        public double Price { get; set; }
+        public bool BooleanMember { get; set; }
+        public ImageSource Image { get; set; }
+    }
+}
