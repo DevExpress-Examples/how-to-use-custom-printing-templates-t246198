75a8a2823d6cd9aafa80c9b8bec073dcc8d72de8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/SLGridExample/My Project/AssemblyInfo.vb
@@ -0,0 +1,44 @@
+﻿' Developer Express Code Central Example:
+' How to use custom printing templates
+' 
+' This example demonstrates how to use grid's printing templates to change the
+' grid printing appearance.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E3228
+
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("SLGridExample")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("SLGridExample")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2011")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ae5851d7-2f6c-4e6e-b7fa-76f3db9cf8e6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Revision and Build Numbers 
+' by using the '*' as shown below:
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
